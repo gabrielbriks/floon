c451daefd19cc8c8e039ccd610407344d5faa7d5
Gabriel Morais <37519878+gabrielbriks@users.noreply.github.com>
2024-07-23 08:43:14 -0300

    edit

--- forge.config.js
+++ forge.config.js
@@ -14,7 +14,7 @@ module.exports = {
           owner: "gabrielbriks",
           name: "floon",
         },
-        prerelease: false,
+        prerelease: true,
         draft: true,
       },
     },
